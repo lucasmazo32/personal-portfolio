14a60258b6a70d40430c7b4589d027bb0a632c99
Lucas Mazo <lucasmazo32@gmail.com>
2020-05-18 16:38:27 -0500

    Initial build subtree commit

---
+++ build/precache-manifest.8cfb5718eb66b67fe871502c2113d218.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f658cf4b5541f86057c13f601796776c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8b57efe441c53a639f45",
+    "url": "/static/css/main.44aa6fac.chunk.css"
+  },
+  {
+    "revision": "af71baca7e6e0db04991",
+    "url": "/static/js/2.d0a80ed3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.d0a80ed3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b57efe441c53a639f45",
+    "url": "/static/js/main.4105b641.chunk.js"
+  },
+  {
+    "revision": "967a46d8b67e4e01db1f",
+    "url": "/static/js/runtime-main.7e4b9196.js"
+  },
+  {
+    "revision": "a9a8d26aad024e67400420b6eb7ba4a8",
+    "url": "/static/media/jlt.a9a8d26a.jpeg"
+  },
+  {
+    "revision": "b670a14da65bf0a3a41e541074b1ea83",
+    "url": "/static/media/march.b670a14d.png"
+  },
+  {
+    "revision": "b02f7fba9eca549273b370311f2a64c5",
+    "url": "/static/media/portfolio.b02f7fba.png"
+  },
+  {
+    "revision": "d4669ac8acde13b3c4d500036c00ab82",
+    "url": "/static/media/profile-photo.d4669ac8.png"
+  },
+  {
+    "revision": "5e72d705930d544f48a07407a3ef8f6f",
+    "url": "/static/media/rails.5e72d705.png"
+  }
+]);
